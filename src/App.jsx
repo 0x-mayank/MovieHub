2a4168c1d1d9b771c559034e65d38414f673eaa2
quantam-r0b0t <mayankm1946@gmail.com>
2025-07-05 22:15:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
+import { MovieProvider } from './context/MovieContext'
+
+function App() {
+  let movieNumber=2
+
+  return (
+    <MovieProvider>
+      <NavBar />
+    <main className="main-content">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+      </main>
+    </MovieProvider>
+  )
+}
+
+export default App
---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = '81f87e51aa74f66509980a4aa2b9496b';
+const BASE_URL = 'https://api.themoviedb.org/3'
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
+
